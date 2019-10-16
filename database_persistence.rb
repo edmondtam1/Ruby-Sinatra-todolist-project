0d358f8e6dca08baf8183888a0b7a87de25e0d79
edmondtam1 <edmondtam2@gmail.com>
2019-10-16 19:08:52 +0100

    Uploading networked application with databases

---
+++ database_persistence.rb
@@ -0,0 +1,86 @@
+require 'pg'
+
+class DatabasePersistence
+  def initialize(logger)
+    @db = if Sinatra::Base.production?
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+          end
+    @logger = logger
+  end
+
+  def disconnect
+    @db.close
+  end
+  
+  def query(statement, *params)
+    @logger.info("#{statement} with #{params}")
+    @db.exec_params(statement, params)
+  end
+
+  def find_list(id)
+    sql = "SELECT * FROM lists WHERE id = $1"
+    result = query(sql, id)
+
+    tuple = result.first
+    { id: tuple['id'], name: tuple['name'],
+      todos: get_todos_from_list(tuple['id']) }
+  end
+
+  def all_lists
+    sql = "SELECT * FROM lists"
+    result = query(sql)
+    result.map do |tuple|
+      list_id = tuple['id'].to_i
+      { id: list_id, name: tuple['name'],
+        todos: get_todos_from_list(list_id) }
+    end
+  end
+
+  def create_new_list(list_name)
+    sql = "INSERT INTO lists(name) VALUES ($1)"
+    query(sql, list_name)
+  end
+
+  def delete_list(id)
+    sql = "DELETE FROM lists WHERE id = $1"
+    query(sql, id)
+  end
+
+  def update_list_name(id, list_name)
+    sql = "UPDATE lists SET name = $1 WHERE id = $2"
+    query(sql, list_name, id)
+  end
+
+  def create_new_todo(list_id, todo_name)
+    sql = "INSERT INTO todos(name, list_id) VALUES ($1, $2)"
+    query(sql, todo_name, list_id)
+  end
+
+  def delete_todo_from_list(list_id, todo_id)
+    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
+    query(sql, todo_id, list_id)
+  end
+
+  def update_todo_status(list_id, todo_id, status)
+    sql = "UPDATE todos SET completed = $1 WHERE id = $2 AND list_id = $3"
+    query(sql, status, todo_id, list_id)
+  end
+
+  def mark_all_todos_as_completed(list_id)
+    sql = "UPDATE todos SET completed = true WHERE list_id = $1"
+    query(sql, list_id)
+  end
+
+  private
+
+  def get_todos_from_list(list_id)
+    sql = "SELECT * FROM todos WHERE list_id = $1"
+    result = query(sql, list_id)
+    result.map do |tuple|
+      { id: tuple["id"], name: tuple["name"],
+        completed: tuple["completed"] == 't' }
+    end
+  end
+end
